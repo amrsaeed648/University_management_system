6d75e703dd1412b562685b6333d8a7a9e1735de0
Essam <eng.essam.mohamed1@gmail.com>
2025-12-02 21:23:40 +0200

    Added addStudent, updateStudent Functions. and Enhanced Searching by getStudentIndex Function

--- project _files/main.cpp
+++ project _files/main.cpp
@@ -8,10 +8,13 @@
 
 using namespace std;
 
+
+/*============================================================Start of Student Class=================================================*/
+
 class Student {
     private:
         static int GlobalStudentCount;
-        string firstName, lastName, id, email, department; 
+        string firstName, lastName, id, email, department;
         int yearOfStudy, studentCount;
         vector<string> enrolledCourses; // course codes
         vector<pair<string, double>> grades; // (courseCode, grade)
@@ -22,9 +25,9 @@ class Student {
             GlobalStudentCount++;
             studentCount = GlobalStudentCount;
             firstName = "NULL";
-            lastName = "NULL"; 
+            lastName = "NULL";
             id = "NULL";
-            email = "NULL"; 
+            email = "NULL";
             department = "NULL";
             yearOfStudy = 0;
             idGeneration();
@@ -33,7 +36,7 @@ class Student {
 
         Student (string fn, string ln, string d, int yos) {
             firstName = fn;
-            lastName = ln; 
+            lastName = ln;
             department = d;
             yearOfStudy = yos;
             idGeneration();
@@ -41,7 +44,7 @@ class Student {
         }
 
         void idGeneration () // ID = XYNNN , X = Department No, Y = YearOfStudy, XXX = Student Number
-        { 
+        {
             int dn;
             if (department == "CSE") dn = 1;
             else if (department == "ECE") dn = 2;
@@ -123,40 +126,14 @@ class Student {
             }
         }
 
-        
+
+
 };
 
-/*==========================================================Start of Student Functions==============================================*/
-
-void deleteStudent(string id) /* it deletes the student with id */ {
-    int index;
-    for (int i = 0; i < students.size(); i++) {
-        if (students[i].getID() == id) index = i;
-    }
-    students.erase(students.begin() + index);
-}
-
-void studentsList() {
-    for (int i = 0; i < students.size(); i++) students[i].info();
-}
-
-/*===========================================================End of Student Functions===============================================*/
+/*============================================================End of Student Class=================================================*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*============================================================Start of Course Class=================================================*/
 
 class Course {
 private:
@@ -187,15 +164,238 @@ public:
 
 };
 
-int Student::GlobalStudentCount = 0;
+/*=============================================================End of Course Class==================================================*/
+
+
+
+
+/*==========================================================Start of Program Variables==============================================*/
 
 vector<Student> students;
 vector<Course> courses;
 
+int Student::GlobalStudentCount = 0;
 
-int main(){
+/*===========================================================End of Program Variables===============================================*/
 
-    return 0;
+
+
+
+/*==========================================================Start of Student Functions==============================================*/
+
+int getStudentIndex(string id) { return stoi(id.substr(2))-1; }
+
+void addStudent () {
+    string fn, ln, d;
+    int yos;
+    cout<<"Please Enter Student Data: (First Name, Last Name, Department, Year of Study)\n";
+    cin>>fn>>ln>>d>>yos;
+    Student st(fn, ln, d, yos);
+    students.push_back(st);
+    cout<<"Student Added Successfully.\n";
+    cout<<"His ID is: "<<st.getID()<<" .\n";
+    cout<<"His Email is: "<<st.getEmail()<<" .\n";
 }
 
-//
+void updateStudent(string id) {
+    int index = getStudentIndex(id);
+    string input; 
+    Student& st = students[index]; 
+    while (true) { 
+        int c; 
+        cout<<"\n====================================================================================================================\n"; 
+        cout<<"================= You are Updating "<<st.getFirstName()<<" "<<st.getLastName()<<"'S Data Now. =================\n"; 
+        cout<<"====================================================================================================================\n"; 
+        cout<<"1. First Name\n2. Last Name\n3. Department\n4. Year of Study\n5. Exit\nPlease Select from Above: "; 
+        cin>>c; if (c == 1) {
+            cout<<"Enter the New Data: "; cin>>input; 
+            st.setFirstName(input); 
+            cout<<" Data Updated Successfully."; 
+        }
+        else if (c == 2) {
+            cout<<"Enter the New Data: "; cin>>input; 
+            st.setLastName(input); 
+            cout<<" Data Updated Successfully."; 
+        }
+        else if (c == 3) {
+            cout<<"Enter the New Data: "; cin>>input; 
+            st.setDepartment(input); 
+            cout<<" Data Updated Successfully."; 
+        }
+        else if (c == 4) {
+            cout<<"Enter the New Data: "; cin>>input; 
+            st.setYearOfStudy(stoi(input)); 
+            cout<<" Data Updated Successfully."; 
+        }
+        else if (c == 5) break; 
+        else cout<<"Invalid Input."; 
+    } 
+}
+
+void deleteStudent(string id) /* it deletes the student with id */ {
+    int index = getStudentIndex(id);
+    students.erase(students.begin() + index);
+}
+
+void studentsList() {
+    for (int i = 0; i < students.size(); i++) students[i].info();
+}
+
+/*===========================================================End of Student Functions===============================================*/
+
+
+
+
+
+
+
+
+
+
+
+int main(){
+    int choice;
+    while (true) {
+        cout << "\n============== University Management System ==============\n";
+        cout << "1. Add Student\n";
+        cout << "2. Delete Student\n";
+        cout << "3. Student Information\n";
+        cout << "4. List All Students\n";
+        cout << "5. Enroll Student in Courses\n";
+        cout << "6. Add Course\n";
+        cout << "7. List All Courses\n";
+        cout << "0. Exit\n";
+        cout << "Choose: ";
+
+        cin >> choice;
+
+        if (choice == 0) {
+            cout << "Exiting...\n";
+            break;
+        }
+
+        if (choice == 1) {
+            string fn, ln, dep;
+            int yos;
+
+            cout << "Enter First Name: ";
+            cin >> fn;
+
+            cout << "Enter Last Name: ";
+            cin >> ln;
+
+            cout << "Enter Department (CSE/ECE/EPE): ";
+            cin >> dep;
+
+            cout << "Enter Year of Study: ";
+            cin >> yos;
+
+            students.push_back(Student(fn, ln, dep, yos));
+
+            cout << "Student Added Successfully!\n";
+            cout << "Your ID: " << students.back().getID() << "\n";
+        }
+
+        else if (choice == 2) {
+            string id;
+            cout << "Enter Student ID to Delete: ";
+            cin >> id;
+            deleteStudent(id);
+            cout << "Deleted Successfully!\n";
+        }
+
+        else if (choice == 3) {
+            string id;
+            cout << "Enter Student ID: ";
+            cin >> id;
+
+            bool found = false;
+
+            for (auto &s : students) {
+                if (s.getID() == id) {
+                    found = true;
+
+                    cout << "\n=== Student Information ===\n";
+                    s.info();
+                    cout << "\n";
+
+                    // cout << "Enrolled Courses:\n";
+                    // s.getEnrolledCourses();
+                    // cout << "\nGrades:\n";
+                    // s.getGrades();
+                    // cout << "\n";
+
+                    break;
+                }
+            }
+
+            if (!found)
+                cout << "Student not found!\n";
+
+
+        }
+
+        else if (choice == 4) {
+            cout << "\n=== Students List ===\n";
+            studentsList();
+            cout << "\n";
+        }
+
+        else if (choice == 5) {
+            string id;
+            cout << "Enter Student ID to Enroll in Courses: ";
+            cin >> id;
+
+            bool found = false;
+            for (auto &s : students) {
+                if (s.getID() == id) {
+                    found = true;
+                    s.enrollCourse();
+                    cout << "Enrollment Completed!\n";
+                }
+            }
+
+            if (!found)
+                cout << "Student not found!\n";
+        }
+
+        else if (choice == 6) {
+            string code, name, prof, dep;
+            int y;
+
+            cout << "Enter Course Code: ";
+            cin >> code;
+
+            cout << "Enter Course Name: ";
+            cin >> name;
+
+            cout << "Enter Professor Name: ";
+            cin >> prof;
+
+            cout << "Enter Department: ";
+            cin >> dep;
+
+            cout << "Enter Year: ";
+            cin >> y;
+
+            courses.push_back(Course(code, name, prof, dep, y));
+            cout << "Course Added!\n";
+        }
+
+        else if (choice == 7) {
+            cout << "\n=== Courses List ===\n";
+            for (auto &c : courses) {
+                cout << "{ " << c.getCode() << " , "
+                     << c.getName() << " , "
+                     << c.getProfessor() << " , "
+                     << c.getDepartment() << " , "
+                     << c.getYear() << " }\n";
+            }
+        }
+
+        else {
+            cout << "Invalid choice! Try again.\n";
+        }
+    }
+    return 0;
+}
