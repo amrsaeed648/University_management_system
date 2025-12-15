04a8a858ea4247c5dbd6cfd2513659e1d4e8d0fc
eng-essam <eng.essam.mohamed1@gmail.com>
2025-12-15 04:50:36 +0200

    Replace enrollStudentInCourse with displayCoursesByStudentId

--- src/studentManagmentMenu.cpp
+++ src/studentManagmentMenu.cpp
@@ -69,7 +69,7 @@ void studentManagementMenu() {
         else if (c == 6){
             clearScreen();
             printBanner();
-            enrollStudentInCourse();
+            displayCoursesByStudentId();
             cin.ignore();
             pauseScreen();
         }
